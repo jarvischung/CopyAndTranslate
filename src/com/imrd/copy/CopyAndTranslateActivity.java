f7dbda638addbbaadd5d8e229f565d0c4f4a3383
weikunlu <weikun.lu@gmail.com>
2013-06-20 14:11:57 +0800

    remove unused network package.

--- src/com/imrd/copy/CopyAndTranslateActivity.java
+++ src/com/imrd/copy/CopyAndTranslateActivity.java
@@ -21,7 +21,6 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 import com.imrd.copy.dict.StarDict;
-import com.imrd.copy.network.NetworkFactory;
 import com.imrd.copy.service.ICountService;
 import com.imrd.copy.service.UpdateService;
 import com.imrd.copy.util.LogProcessUtil;
@@ -43,12 +42,6 @@ public class CopyAndTranslateActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_copy);
-
-		/*
-		String url = String.format(getString(R.string.translate_url),
-				this.sourceLang, this.desLang, this.text);
-		new NetworkFactory(url, NetworkFactory.GET).start();
-		 */
 		
 		final Intent intent = new Intent(CopyAndTranslateActivity.this,
 				UpdateService.class);
